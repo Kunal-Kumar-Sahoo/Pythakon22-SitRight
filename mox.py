81a14d7ffcd903e470c87c4a1776f8c13f2e698a
Kishan Pipariya <intelkishan@gmail.com>
2022-08-05 19:46:17 +0530

    Notification integrated with posture detection

--- mox.py
+++ mox.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import os
+import time
 
 url = "https://api.pushbullet.com/v2/pushes"
 data = {'type': 'note', 'title': 'alert', 'body': 'Incorrect posture'}
@@ -9,6 +11,10 @@ sess.headers.update({'Content-Type': 'application/json'})
 
 data = {'type': 'note',
         'title': 'Alert',
-        'body': 'Your posture has been wrong for over a minute'}
-r = sess.post('https://api.pushbullet.com/v2/pushes', data=json.dumps(data))
-print(r.text)
+        'body': 'Your posture has been wrong for over 5 minutes'}
+while True:
+    file = os.listdir('.')
+    if 'warning.txt' in file:
+        r = sess.post('https://api.pushbullet.com/v2/pushes', data=json.dumps(data))
+        os.remove('./warning.txt')
+
